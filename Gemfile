142c3f521f50950a760b39d641de1a6e64b39817
cock1doodledoo <cock1doodledoo@gmail.com>
2015-08-14 16:09:44 +0900

    did not work after update capistrano 3.3.5 -> 3.4

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'capistrano'
+gem 'capistrano', '< 3.4.0'
 gem 'itamae'
 gem 'serverspec'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,7 @@
 require 'yaml'
 
-# config valid only for Capistrano 3.3
+# config valid only for Capistrano 3.3.5
 lock '3.3.5'
 
 set :pty, true
 
-# default variables
-set :default_vars, YAML.load_file('config/default_vars.yml').each_with_object({}){|(k,v),memo| memo[k.to_s.to_sym]=v}
